4aca9bd88a9b2304d746f4e16249305852008ece
Leandro Leite <lleitep3@gmail.com>
2012-02-04 18:50:30 -0200

    #2 funcionalidade de criar elementos ja esta ok

--- index.php
+++ index.php
@@ -2,12 +2,35 @@
     <head>
         <title>UI PDF Creator</title>
         <link rel="stylesheet" href="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.17/themes/base/jquery-ui.css" type="text/css" media="all">
+        <link rel="stylesheet" href="css/style.css" type="text/css" media="all">
         <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js" type="text/javascript"></script>
         <script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.17/jquery-ui.min.js" type="text/javascript"></script>
+<!--        <script src="js/jquery-1.7.1.min.js" type="text/javascript"></script>
+        <script src="js/jquery-ui-1.8.17.custom.min.js" type="text/javascript"></script>-->
 
         <script type="text/javascript">
             $(function(){
-                $('.component').draggable({containment : 'parent'});
+                $('.component').draggable({ helper: "clone", revert : "invalid" });
+                $('#paper').droppable({
+                    drop: function( event, ui ) {
+                        if($(ui.draggable).hasClass('component')){
+                            var $el = $(ui.draggable)
+                                      .clone()
+                                      .removeClass('component')
+                                      .css('top', ui.position.top,'left')
+                                      .addClass('valid element');
+                            
+                            $(this).append($el);
+                            selectElement($el);
+                            $('.valid').draggable({ containment : 'parent' });
+                        }
+                    }
+                });
+                
+                function selectElement($el){
+                    $('#paper .valid').removeClass('selected');
+                    $el.addClass('selected');
+                }
             });
         </script>
 
@@ -18,17 +41,13 @@
             <section id="esquerda">
                 <nav id="menu">
                     <ul>
-                        <li>Text Element</li>
-                        <li>Image Element</li>
+                        <li><div class="component">Text</div></li>
+                        <li><div class="component">Imagem</div></li>
                     </ul>
                 </nav>    
             </section>
             <section id="center">
-                <div id="paper">
-                    <div class="component">
-
-                    </div>
-                </div>
+                <div id="paper"></div>
             </section>
         </section>
         <footer>
